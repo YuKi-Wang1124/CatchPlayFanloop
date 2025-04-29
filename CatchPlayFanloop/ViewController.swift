850e60bf3791ad75edc076b1aa927a73e506b20a
mandy <mandy@newpos.tw>
2025-04-29 18:05:26 +0800

    [init] Initial commit for CatchPlayFanloop project

---
+++ CatchPlayFanloop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchPlayFanloop
+//
+//  Created by 王昱淇 on 2025/4/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CatchPlayFanloopTests/CatchPlayFanloopTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchPlayFanloopTests.swift
+//  CatchPlayFanloopTests
+//
+//  Created by 王昱淇 on 2025/4/29.
+//
+
+import Testing
+@testable import CatchPlayFanloop
+
+struct CatchPlayFanloopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
